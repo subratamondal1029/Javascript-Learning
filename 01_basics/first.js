9b84bd33d04692ab9e284de561a1562f27610617
Subrata Mondal <subratamondal1029@outlook.com>
2024-04-09 22:11:14 +0530

    Learning Variables of javascript

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// console.log('hello world');
+
+const age = 20; //const cant changeable
+let village = 'alichak';  //it could be changeable
+var name = 'subrata mondal';  //it could be changeable, but we don't prefer to use var instead of let and const.
+
+console.table([age,village, name]);
+
+// age = 10; //const cant changeable
+village = 'patna'; // could be changeable.
+name = 'Raja'; //it could be changeable
+
+console.table([age,village, name]);
+
+profation = 'programer'; //you can also define a variable like this but we don't prefer defining like this.
+console.log(profation);
