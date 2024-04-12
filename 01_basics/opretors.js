11ed7625c902fb881c3d61fdfdf0910db8cb71bb
subrata mondal <subratamondal1029@outlook.com>
2024-04-12 08:25:17 +0530

    learning oprations in js

---
+++ 01_basics/opretors.js
@@ -0,0 +1,21 @@
+// lets talk about the oprators
+
+// Basic oprations
+
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1/1);
+console.log(1%1);
+
+// string oprations
+const str1 = "string 1"
+const str2 = " string 2"
+const combinestr = str1 + str2;
+console.log(combinestr); //=> string 1 string 2
+
+// string and number oprations
+console.log("1" + 1); // 11
+console.log(1 + "1"); // 11
+console.log("1" + 1 + 1); // 111
+console.log(1 + 1 + "1"); // 21
