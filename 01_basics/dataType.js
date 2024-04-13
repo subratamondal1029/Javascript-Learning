06a2c17a14c5957a7ca14350b98e05f4919b828d
subrata mondal <subratamondal1029@outlook.com>
2024-04-13 20:01:40 +0530

    revise the Data types  in js

--- 01_basics/dataType.js
+++ 01_basics/dataType.js
@@ -1,17 +1,17 @@
 // There are some data type in javascript
 
-// premative data types 
+// premative data types
 
 // String => "This is a string" //If you write anything in "" This gone be string
 // Number => 20 //If you write anything in number it will be number
 // Boolean => true //If you write anything in boolean(true/false) it will be boolean
-// undefined => let state; //If you Don't giv any values in a variable it will be undefined 
+// undefined => let state; //If you Don't giv any values in a variable it will be undefined
 // null => let state = null; //null is a standalone value You have to define that value
-// BigInt => 9007199254740991 //BigInt is use for big numbers
+// BigInt => 9007199254740991n //BigInt is use for big numbers
 // Symbol => symbol is used to define unique identifiers
 
-
-// none premative Data Types 
+// none premative Data Types
 
 // Object => {name: "subrata mondal", age: 17, isActive = true} => object is use to define multiple key and values If we use object we don't need to write each variable.
 // Array => ["subrata mondal", 17, true] => array is same as object But we dont need to write key we write only values;
+//functions => function(){}
---
+++ 01_basics/datatypeSammary.js
@@ -0,0 +1,46 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n;
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "hitesh",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+myFunction();
+
+console.log("number == " + typeof score);
+console.log("decimal number == " + typeof scoreValue);
+console.log("boolean == " + typeof isLoggedIn);
+console.log("null == " + typeof outsideTemp);
+console.log("undefined == " + typeof userEmail);
+console.log("symbol == " + typeof id, typeof anotherId);
+console.log("bigint == " + typeof bigNumber);
+console.log("array == " + typeof heros);
+console.log("object == " + typeof myObj);
+console.log("function == " + typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
