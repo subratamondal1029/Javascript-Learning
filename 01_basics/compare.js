aed36d705c26c0ad8559fa528b7b9fb1956e5701
subrata mondal <subratamondal1029@outlook.com>
2024-04-13 19:43:28 +0530

    learning comparision  in js

---
+++ 01_basics/compare.js
@@ -0,0 +1,22 @@
+//Basic comparition
+// console.log(1==1);
+// console.log(1!=1);
+// console.log(1>1);
+// console.log(1<1);
+// console.log(1<=1);
+// console.log(1>=1);
+
+// string and number comparison
+// console.log("2" > 1);                         // javascript convert string to number
+// console.log("02" > 1);
+
+// bellow code not a good practice
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === strict check(data type comparison)
+// console.log("2" === 2);
