1355e410eb61df7095fa73a4c13cf6e989610719
subrata mondal <subratamondal1029@outlook.com>
2024-05-12 19:59:21 +0530

    xhr is complete and build projet is complete

---
+++ 07_advanced/Xhr_request.js
@@ -0,0 +1,19 @@
+// xhr is a old mathod to get Data from api
+// xhr is not work in node js invironment its work in browser (This file link with api.html)
+
+const apiUrl = "https://api.github.com/users/subratamondal1029";
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", apiUrl);
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    const data = JSON.parse(this.responseText) // we have to convert data to a json object because this is comes as string string
+        console.log(data);
+  }
+};
+
+document.querySelector('button').addEventListener('click', () => xhr.send())
+// we can directly call xhr.send()
+
+// Project in project.html
