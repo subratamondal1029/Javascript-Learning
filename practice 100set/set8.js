8b17da2b20fc1486715f6ac09abfef4d3d682a73
subrata mondal <subratamondal1029@outlook.com>
2024-05-16 20:42:45 +0530

    adding one more way to do this

--- practice 100set/set8.js
+++ practice 100set/set8.js
@@ -1,5 +1,8 @@
 function findMax(arr){
-    return arr.sort((a, b) => b-a)[0]
+    // return arr.sort((a, b) => b-a)[0]
+
+    // one more way
+    return Math.max(...arr) // using spread oprator (...) the value will pass as numbers one by one
 }
 
 console.log(findMax([6]));
