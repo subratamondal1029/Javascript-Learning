924c51f35b4dcf7b4a9eb4682253df4e4fa1df48
subrata mondal <subratamondal1029@outlook.com>
2024-05-17 08:46:23 +0530

    set 9 practice is complete

---
+++ practice 100set/set9.js
@@ -0,0 +1,14 @@
+// Find factorial number
+function findFactorial(n){
+    let factorial = 1
+    for (let i = 1; i <=n; i++) {
+       factorial *= i
+        
+    }
+    console.log(factorial);
+}
+
+
+findFactorial(5)
+findFactorial(2)
+findFactorial(4)
